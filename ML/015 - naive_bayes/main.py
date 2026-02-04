54ead7ce813e9f7375c50f56be7f20688696806a
Chillmav <jakub.kulig2004@gmail.com>
2026-02-04 15:12:47 +0100

    naive_bayes

---
+++ ML/015 - naive_bayes/main.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+
+df = pd.read_csv("ML/data/spam.csv", encoding="latin-1")
+data = df[["v1", "v2"]].values
+
+def split_dataset(data):
+
+    size = data.shape[0]
+    train_data_indices = np.random.choice(size, int(np.round(size * 4/5)), replace=False)
+    test_data = np.delete(data, train_data_indices, axis=0)
+    train_data = data[train_data_indices]
+    return train_data, test_data
+
+train_data, test_data = split_dataset(data)
+
+class NaiveBaiseClassifier:
+
+    def __init__(self, train_data, alpha=1):
+
+        self.train_texts = train_data[:, 1]
+        self.train_labels = train_data[:, 0]
+        self.spam_dict = {}
+        self.legit_dict = {}
+        self.spam_priori = 0 
+        self.legit_priori = 0 
+        self.total_spam_words = 0
+        self.total_legit_words = 0
+        self.alpha = alpha  
+        self.total_vocab = 0
+    def fit(self):
+
+        for (text, label) in zip(self.train_texts, self.train_labels):
+
+            words = text.split(" ")
+
+            if (label == "ham"):
+                for word in words:
+                    self.legit_dict[word] = self.legit_dict.get(word, 0) + 1
+            else:
+                for word in words:
+                    self.spam_dict[word] = self.spam_dict.get(word, 0) + 1
+
+        counter = Counter(self.train_labels)
+        self.spam_priori = counter.get("spam") / (counter.get("spam") + counter.get("ham"))
+        self.legit_priori = 1 - self.spam_priori
+        self.total_spam_words = sum(self.spam_dict.values())
+        self.total_legit_words = sum(self.legit_dict.values())
+        vocab = set(self.spam_dict) | set(self.legit_dict) # union
+        self.total_vocab = len(vocab)
+
+    def predict(self, test_data):
+
+        predictions = []
+        for text in test_data:
+            predictions.append(self.predict_label(text))
+
+        return predictions
+    
+    def validate(self, labels, predictions):
+
+        guessed = 0
+        for (label, prediction) in zip(labels, predictions):
+
+            if (label == prediction):
+                guessed += 1
+
+        return guessed / len(labels)
+        pass
+
+    def predict_label(self, text):
+
+        spam_likelyhood, legit_likelyhood = np.log(self.spam_priori), np.log(self.legit_priori)
+        words = text.split(" ")
+        for word in words:
+
+            spam_likelyhood += np.log((int(self.spam_dict.get(word, 0)) + self.alpha) / (self.total_vocab * self.alpha + self.total_spam_words))
+            legit_likelyhood += np.log((int(self.legit_dict.get(word, 0)) + self.alpha) / (self.total_vocab * self.alpha + self.total_legit_words))
+
+        if (spam_likelyhood > legit_likelyhood):
+            return "spam"
+        
+        return "ham"
+        
+
+model = NaiveBaiseClassifier(train_data)
+model.fit()
+predictions = model.predict(test_data[:, 1])
+accuracy = model.validate(test_data[:, 0], predictions)
+print(accuracy)
+
--- algebra/main.py
+++ algebra/main.py
@@ -1,18 +1,31 @@
 import numpy as np
 import scipy.linalg as la
 
+def inv(A: np.ndarray): # matrix 3x3
 
-A = np.array([
-    [1, -0.05, -0.3],
-    [-0.01, 1, -0.01],
-    [-0.1, 0, 1]
-])
+    rows = A.shape[0]
+    cols = A.shape[1]
+    C = np.zeros((rows, cols))
 
-B = np.array([
-    [25],
-    [10],
-    [14]
-], dtype=float)
+    if np.linalg.det(A) != 0:
 
-y = la.solve(A, B)
-print(y)
+        for i in range(A.shape[0]):
+            for j in range(A.shape[1]):
+                C[i, j] = np.linalg.det(np.c_[np.vstack([A[0:i], A[i+1:rows]])[:, 0:j], np.vstack([A[0:i], A[i+1:rows]])[:, j+1:cols]]) * (-1)**(i+j)
+    
+    else:
+        
+        print("Inverse doesn't exist")
+        return 0
+    
+    inv = 1/np.linalg.det(A) * C.T
+    return inv
+inverse = inv(np.array([[1, 4, 3],
+              [2, 0, 1],
+              [0, -2, -1]
+              ]))
+print(inverse)
+print(np.linalg.inv(np.array([[1, 4, 3],
+              [2, 0, 1],
+              [0, -2, -1]
+              ])))
