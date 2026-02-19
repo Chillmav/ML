204ab8cc8c87da4661090c244a54c219662f0773
Chillmav <jakub.kulig2004@gmail.com>
2026-02-19 20:53:35 +0100

    binary_logistic_regression + softmax

--- ML/024 - cross_entropy and maximum_likelihood/002 - binary_logistic_regression/main.py
+++ ML/024 - cross_entropy and maximum_likelihood/002 - binary_logistic_regression/main.py
@@ -1,4 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+X0 = np.random.normal(0, 1, 400)
+X1 = np.random.normal(3, 1, 600)
+X = np.concatenate([X0, X1])
+Y_0 = np.zeros(400)
+Y_1 = np.ones(600)
+Y = np.concatenate([Y_0, Y_1])
+data = np.c_[X, Y]
+def split_dataset(data: np.ndarray):
 
+    size = data.shape[0]
+    train_data_indices = np.random.choice(size, int(np.round(size * 4/5)), replace=False)
+    test_data = np.delete(data, train_data_indices, axis=0)
+    train_data = data[train_data_indices]
+    return train_data, test_data
+
+train_data, test_data = split_dataset(data)
+X_train, Y_train, X_test, Y_test = train_data[:, :-1], train_data[:, -1], test_data[:, :-1], test_data[:, -1]
+
+
+
+class LogisticRegression:
+
+    def __init__(self, epoch=1000, lr=0.01):
+        
+        self.theta = np.array([np.random.normal(-1, 1), np.random.normal(-1, 1)])
+        self.epochs = 1000
+        self.lr = lr
+
+    def train(self, X, Y):
+        
+        for e in range(self.epochs):
+
+            error = Y - self.sigmoid(X)
+
+            self.theta[0] += self.lr * np.mean(error)
+            for w in range(len(self.theta[1:])):
+                self.theta[w+1] += self.lr * np.mean(X[:, w] * error)
+        
+    def test(self, X, Y):
+
+        n = len(Y)
+        predicted = np.round(self.sigmoid(X)).flatten()
+        x_n = np.linspace(np.min(X), np.max(X), n)
+        plt.scatter(X, Y)
+        plt.plot(x_n, self.sigmoid(x_n))
+        plt.show()
+
+
+        return np.sum(predicted == Y) / n
+
+    def sigmoid(self, X):
+
+        p_X = 1 / (1 + np.exp(-(self.theta[0] + self.theta[1]*X)))
+
+        return p_X
+        
+    
+model = LogisticRegression()
+
+model.train(X_train, Y_train)
+print(model.test(X_test, Y_test))
---
+++ ML/024 - cross_entropy and maximum_likelihood/003 - softmax_regression/main.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+X0 = np.random.normal(0, 1, 400).reshape(-1, 1)
+X1 = np.random.normal(3, 1, 600).reshape(-1, 1)
+X2 = np.random.normal(5, 1, 400).reshape(-1, 1)
+X = np.concatenate([X0, X1, X2])
+Y_0 = np.zeros(400)
+Y_1 = np.ones(600)
+Y_2= np.full(400, 2)
+Y = np.concatenate([Y_0, Y_1, Y_2])
+
+data = np.c_[X, Y]
+
+def split_dataset(data: np.ndarray):
+
+    size = data.shape[0]
+    train_data_indices = np.random.choice(size, int(np.round(size * 4/5)), replace=False)
+    test_data = np.delete(data, train_data_indices, axis=0)
+    train_data = data[train_data_indices]
+    return train_data, test_data
+
+train_data, test_data = split_dataset(data)
+X_train, Y_train, X_test, Y_test = train_data[:, :-1], train_data[:, -1], test_data[:, :-1], test_data[:, -1]
+
+
+class SoftmaxRegression:
+
+    def __init__(self, epochs = 1000, lr=0.01):
+
+        self.epochs = 1000
+        self.theta = []
+        self.lr = lr
+        self.labels = 0
+
+    def train(self, X, Y):
+
+        self.labels = len(np.unique(Y))
+        self.theta = np.zeros((self.labels, X.shape[1] + 1))
+
+        for l in range(self.labels):
+            self.theta[l] = np.random.uniform(-1, 1, size=X.shape[1] + 1)
+
+        print(self.softmax(X))
+        for e in range(self.epochs):
+            pass
+
+            
+    def softmax(self, X):
+
+        P = np.zeros((X.shape[0], self.labels))
+
+        for i in range(X.shape[0]):
+
+            z = np.zeros(self.labels)
+
+            # logits
+            for l in range(self.labels):
+                z[l] = self.theta[l][0] + np.dot(self.theta[l][1:], X[i])
+
+            # stabilność
+            z = z - np.max(z)
+
+            exp_z = np.exp(z)
+
+            P[i] = exp_z / np.sum(exp_z)
+
+        return P
+    
+model = SoftmaxRegression()
+
+model.train(X_train, Y_train)
