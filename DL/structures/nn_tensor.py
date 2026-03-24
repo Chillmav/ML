broadcasting to fix

DL/structures/nn_tensor.py
@@ -1,82 +1,79 @@
 import numpy as np
 import math
 from value import Value
-
-class Neuron:
-
-    def __init__(self, inputs: int, is_lin=False):
-
-        self.w = [Value(np.random.uniform(-0.1, 0.1)) for _ in range(inputs)]
-        self.b = Value(np.random.uniform(-0.1, 0.1))
-        self.is_lin = is_lin
-
-    def __call__(self, x: np.ndarray):
-
-        y = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
-        return y.sigmoid() if not self.is_lin else y
-
-    def parameters(self):
-
-        return self.w + [self.b]
+from tensor import Tensor
+from data import X, y
 
 class Layer:
 
-    def __init__(self, inputs: int, outputs: int): # outputs -> number of neurons in layer
+    def __init__(self, inputs: int, outputs: int, is_lin=False): # outputs -> number of neurons in layer
 
-        self.neurons = [Neuron(inputs) for _ in range(outputs)]
-    
-    def __call__(self, x: np.ndarray):
+        self.W = Tensor(data=np.random.uniform(-0.1, 0.1, (inputs, outputs)))
+        self.b = Tensor(data=np.random.uniform(-0.1, 0.1, (1, outputs)))
+        self.is_lin = is_lin
 
-        outs = [neuron(x) for neuron in self.neurons]
-        return outs[0] if len(outs) == 1 else outs
+    def __call__(self, X):
+
+        if not isinstance(X, Tensor):
+            X = Tensor(X)
+
+        Y = X @ self.W + self.b
+        return Y.relu() if not self.is_lin else Y
     
     def parameters(self):
 
-        return [p for neuron in self.neurons for p in neuron.parameters()]
+        return [self.W, self.b]
     
 class MLP:
 
     def __init__(self, inputs: int, n_outputs: np.ndarray): # n_outputs -> list of numbers of neurons in layers
-
+        
         sz = [inputs] + n_outputs
         self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(sz) - 1)]
     
-    def __call__(self, x: np.ndarray):
+    def __call__(self, X: np.ndarray):
 
         for layer in self.layers:
-            x = layer(x)
-        return x
-    
+            X = layer(X)
+        return X
+
+    def linear_output(self):
+
+        self.layers[-1].is_lin = True
+
     def parameters(self):
 
         return [p for layer in self.layers for p in layer.parameters()]
 
-    def linear_output(self):
+    def zero_grad(self):
 
-        for neuron in self.layers[-1].neurons: neuron.is_lin = True
+        for layer in self.layers:
 
+            layer.W.grad = np.zeros_like(layer.W.grad)
+            layer.b.grad = np.zeros_like(layer.b.grad)
+    
+    def SSE(self, y_real, y_predicted: Tensor):
+
+        return ((y_predicted - y_real)**2).sum()
+    
+    def train(self, X, y, epoch=1000, lr=0.01):
+
+        X = Tensor(X) if not isinstance(X, Tensor) else X
+        y = Tensor(y) if not isinstance(y, Tensor) else y
+
+        for e in range(epoch):
+
+            y_pred = self(X)
+            loss = self.SSE(y_real = y, y_predicted=y_pred)
+            if (e % 20 == 0):
+                print(f"{e} : {loss.data}")
             
+            self.zero_grad()
 
-xs = [[2.0, 3.0, -1.0], [3.0, -1.0, 0.5], [0.5, 1.0, 1.0], [1.0, 1.0, -1.0]]
-ys = [1.0, 0, 0, 1.0] #desired outputs
+            loss.backward()
+            for param in self.parameters():
+                param.data -= param.grad * lr
 
-nn = MLP(3, [4, 4, 1])
-nn.linear_output()
-lr = 0.1
+nn = MLP(4, [10, 10, 1])
+nn.train(X, y)
 
-for e in range(300):
-
-    y = [nn(x) for x in xs]
-    loss = sum([(y_r - y_p)**2 for y_r, y_p in zip(ys, y)])
-    if (e % 20 == 0):
-        print(f"{e} : {loss.data}")
-
-    for p in nn.parameters():
-        p.grad = 0.0
-
-    loss.backward()
-    for param in nn.parameters():
-        param.data -= param.grad * lr
-        
-print(ys)
-print([nn(x).data for x in xs])

DL/structures/tensor.py
@@ -3,30 +3,16 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-X = [
-    [2.0, 3.0, -1.0],
-    [3.0, -1.0, 0.5],
-    [0.5, 1.0, 1.0],
-    [1.0, 1.0, -1.0]
-] # 4 x 3
-
-# weight matrix:
-
-# W =
-# [
-#  [w11 w12 w13],
-#  [w21 w22 w23],
-#  [w31 w32 w33]
-# ] 1 neuron -> 1 column
-
-
 class Tensor:
 
     def __init__(self, data, _children=(), _op='', label=''):
 
         self.data = np.array(data) if not isinstance(data, np.ndarray) else data # (batch_size, features)
-        self.grad = np.zeros(self.data.shape)
+
+        if self.data.shape == ():
+            self.data = self.data.reshape(1, 1)
+
+        self.grad = np.zeros_like(self.data, dtype=np.float64)
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -62,10 +48,19 @@ class Tensor:
         out = Tensor(self.data * other.data, (self, other), '*')
         def _backward():
             self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
-        out._backward = _backward
+
+            grad_other = self.data * out.grad
+
+            if other.grad.shape != grad_other.shape:
+                grad_other = grad_other.sum(axis=0, keepdims=True)
+
+            other.grad += grad_other
 
         return out
+    
+    def __rmul__(self, other):
+
+        return self * other
     
     def __matmul__(self, other: Tensor):
         
@@ -108,11 +103,11 @@ class Tensor:
         
         n = self.data
         e = np.exp(2*n)
-        t = (e - np.ones_like(self.data.shape)) / (e + np.ones_like(self.data.shape))
+        t = (e - np.ones_like(self.data)) / (e + np.ones_like(self.data))
         out = Tensor(t, (self, ), "tanh")
 
         def _backward():
-            self.grad += (np.ones_like(self.data.shape) - (t**2)) * out.grad
+            self.grad += (np.ones_like(self.data) - (t**2)) * out.grad
         
         out._backward = _backward
             
@@ -125,7 +120,7 @@ class Tensor:
         out = Tensor(t, (self, ), "sigmoid")
 
         def _backward():
-            self.grad += t * (np.ones_like(self.data.shape) - t) * out.grad
+            self.grad += t * (np.ones_like(self.data) - t) * out.grad
         
         out._backward = _backward
 
@@ -146,7 +141,7 @@ class Tensor:
 
         build_topo(self)
 
-        self.grad = np.ones_like(self.data.shape)
+        self.grad = np.ones_like(self.data)
 
         for node in reversed(topo):
             node._backward()
@@ -158,7 +153,7 @@ class Tensor:
         out = Tensor(np.sum(self.data), (self, ), 'sum')
 
         def _backward():
-            self.grad += np.ones_like(self.data.shape) * out.grad
+            self.grad += np.ones_like(self.data) * out.grad
         
         out._backward = _backward
 